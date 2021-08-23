c1bfa6cb6246166c58476862191ec7f20dc87003
Muneeb <thinkmuneeb@gmail.com>
2021-08-23 22:59:33 +0500

    deployed

--- migrations/1_initial_migration.js
+++ migrations/1_initial_migration.js
@@ -3,5 +3,8 @@ const CoolCars = artifacts.require('CoolCars');
 
 module.exports = async deployer => {
   await deployer.deploy(Migrations);
-  await deployer.deploy(CoolCars, 'Cool Cars', 'BP', 10, 'IU');
+  await deployer.deploy(CoolCars, 'Cool Cars', 'CC', 10, 1629740587);
 };
+
+// 11PM, 23-8-21,
+// https://rinkeby.etherscan.io/address/0x1637071826B4FF55e89547eFD57f9f8C16D63952#contracts
--- truffle-config.js
+++ truffle-config.js
@@ -1,28 +1,8 @@
-/**
- * Use this file to configure your truffle project. It's seeded with some
- * common settings for different networks and features like migrations,
- * compilation and testing. Uncomment the ones you need or modify
- * them to suit your project as necessary.
- *
- * More information about configuration can be found at:
- *
- * trufflesuite.com/docs/advanced/configuration
- *
- * To deploy via Infura you'll need a wallet provider (like @truffle/hdwallet-provider)
- * to sign your transactions before they're sent to a remote public node. Infura accounts
- * are available for free at: infura.io/register.
- *
- * You'll also need a mnemonic - the twelve word phrase the wallet uses to generate
- * public/private key pairs. If you're publishing your code to GitHub make sure you load this
- * phrase from a file you've .gitignored so it doesn't accidentally become public.
- *
- */
-
-// const HDWalletProvider = require('@truffle/hdwallet-provider');
-// const infuraKey = "fj4jll3k.....";
-//
-// const fs = require('fs');
-// const mnemonic = fs.readFileSync(".secret").toString().trim();
+var HDWalletProvider = require('@truffle/hdwallet-provider');
+require('dotenv').config();
+const MNEMONIC = process.env.MNEMONIC;
+const token = process.env.INFURA_TOKEN;
+const etherscanKey = process.env.ETHERSCAN_KEY;
 
 module.exports = {
   /**
@@ -43,10 +23,18 @@ module.exports = {
     // options below to some value.
     //
     development: {
-     host: "127.0.0.1",     // Localhost (default: none)
-     port: 8545,            // Standard Ethereum port (default: none)
-     network_id: "*",       // Any network (default: none)
+      host: '127.0.0.1', // Localhost (default: none)
+      port: 8545, // Standard Ethereum port (default: none)
+      network_id: '*', // Any network (default: none)
     },
+
+    rinkeby: {
+      provider: () =>
+        new HDWalletProvider(MNEMONIC, 'https://rinkeby.infura.io/v3/' + token),
+      network_id: '4',
+      skipDryRun: true,
+    },
+
     // Another network with more advanced options...
     // advanced: {
     // port: 8777,             // Custom port
@@ -82,7 +70,7 @@ module.exports = {
   // Configure your compilers
   compilers: {
     solc: {
-      version: "0.8.0",    // Fetch exact version from solc-bin (default: truffle's version)
+      version: '0.8.0', // Fetch exact version from solc-bin (default: truffle's version)
       // docker: true,        // Use "0.5.1" you've installed locally with docker (default: false)
       // settings: {          // See the solidity docs for advice about optimization and evmVersion
       //  optimizer: {
@@ -91,7 +79,7 @@ module.exports = {
       //  },
       //  evmVersion: "byzantium"
       // }
-    }
+    },
   },
 
   // Truffle DB is currently disabled by default; to enable it, change enabled: false to enabled: true
@@ -101,6 +89,10 @@ module.exports = {
   // $ truffle migrate --reset --compile-all
 
   db: {
-    enabled: false
-  }
+    enabled: false,
+  },
+  plugins: ['truffle-plugin-verify'],
+  api_keys: {
+    etherscan: etherscanKey,
+  },
 };
